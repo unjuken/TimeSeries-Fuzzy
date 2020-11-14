d8cf25fb7bed62808dacb9084d063ab7f09cbe2c
Andres Felipe Ramirez <unjuken@gmail.com>
2020-11-14 16:38:54 -0500

    Added Notebook using FTSImpl

--- FTSImpl.py
+++ FTSImpl.py
@@ -4,12 +4,17 @@ from pyFTS.partitioners import Grid
 from pyFTS.common import FLR
 from pyFTS.models import chen
 
+
 class FuzzyTS:
     def __init__(self, trainData, parts, fuzzyMethod, fuzzyMode):
 
-        self.data = trainData
-        self.fs = Grid.GridPartitioner(data=data,npart=parts)
-        self.fuzzyfied = fs.fuzzyfy(data, method=fuzzyMethod, mode=fuzzyMode)
-        self.patterns = FLR.generate_non_recurrent_flrs(fuzzyfied)
-        self.model = chen.ConventionalFTS(partitioner=fs)
-        self.model.fit(data)
+        self.trainData = trainData
+        self.fs = Grid.GridPartitioner(data=self.trainData,npart=parts)
+        self.fuzzyfied = self.fs.fuzzyfy(self.trainData, method=fuzzyMethod, mode=fuzzyMode)
+        self.patterns = FLR.generate_non_recurrent_flrs(self.fuzzyfied)
+        self.model = chen.ConventionalFTS(partitioner=self.fs)
+        self.model.fit(self.trainData)
+    
+    def predict(self, data, steps_ahead = 1):
+        self.forecasts = self.model.predict(data, steps_ahead)
+        return self.forecasts
--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import timeseries as ts
 import FTSImpl as FTS
 import numpy
+import numpy as np
+import matplotlib.pylab as plt
 
-sarima1000007 = ts.TimeSeries((1, 0, 0), (0, 0, 0, 7))
-'''sarima0100007 = ts.TimeSeries((0, 1, 0), (0, 0, 0, 7))
+'''sarima1000007 = ts.TimeSeries((1, 0, 0), (0, 0, 0, 7))
+sarima0100007 = ts.TimeSeries((0, 1, 0), (0, 0, 0, 7))
 sarima0010007 = ts.TimeSeries((0, 0, 1), (0, 0, 0, 7))
-sarima0001007 = ts.TimeSeries((0, 0, 0), (1, 0, 0, 7))
+sarima0001007 = ts.TimeSeries((0, 0, 0), (1, 0, 0, 7))'''
 sarima0000107 = ts.TimeSeries((0, 0, 0), (0, 1, 0, 7))
-sarima0000017 = ts.TimeSeries((0, 0, 0), (0, 0, 1, 7))
+'''sarima0000017 = ts.TimeSeries((0, 0, 0), (0, 0, 1, 7))
 test_data = sarima1000007.test_data
 
 
@@ -19,4 +21,13 @@ print("(0, 0, 0), (0, 1, 0, 7): RMSE = {rmse}, MAPE = {mape}".format(rmse=sarima
 print("(0, 0, 0), (0, 0, 1, 7): RMSE = {rmse}, MAPE = {mape}".format(rmse=sarima0000017.RMSE, mape=sarima0000017.MAPE))'''
 
 
-FTS.FuzzyTS(sarima1000007.train_data, 10, 'maximum', 'sets')
+trainMtx = numpy.matrix(sarima0000107.train_data).transpose()
+testMtx = numpy.matrix(sarima0000107.test_data).transpose()
+train = numpy.array(trainMtx.flatten())[0]
+test = numpy.array(testMtx.flatten())[0]
+
+fuzzyTS  = FTS.FuzzyTS(train, 10, 'maximum', 'sets')
+
+fig, ax = plt.subplots(nrows=1, ncols=1, figsize=[15,5])
+fuzzyTS.fs.plot(ax)
+fuzzyTS.fs
